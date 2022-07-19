1b3e868584d96d37cd6e0652013b4ca689b09f45
Henry Rincon <henryrinconsantana@gmail.com>
2022-07-18 21:43:21 -0500

    Se crea proyecto

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 // MIS COMPONENTES
 import { AppRouter } from "./router";
 import { store } from "./store";
@@ -9,9 +9,11 @@ export const CalendarApp = () => {
   // RENDER
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
+      <HashRouter>
         <AppRouter />
-      </BrowserRouter>
+      </HashRouter>
+      {/* </BrowserRouter> */}
     </Provider>
   );
 };
